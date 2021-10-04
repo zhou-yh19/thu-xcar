3f918cf86fd1d9b83a8d74c1d9f2a5925be3a199
yangbo <yangb17@mails.tsinghua.edu.cn>
2021-10-04 18:44:45 +0800

    remove build and devel

--- build/xcar-hardware/vesc/vesc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/thu-Xcar/src/xcar-hardware/vesc/vesc
-# Build directory: /home/nvidia/thu-Xcar/build/xcar-hardware/vesc/vesc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
